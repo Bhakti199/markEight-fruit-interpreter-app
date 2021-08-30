ae2f6fd15f4d7b8c231d13e88f6d167d5dff314e
Bhakti199 <78597483+Bhakti199@users.noreply.github.com>
2021-08-30 16:26:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import "./styles.css";
+const fruitDictionary = {
+  "🍇": "Grapes",
+  "🍈": "Melon",
+  "🍉": "Watermelon",
+  "🍊": "Tangerine",
+  "🍋": "Lemon",
+  "🍌": "Banana",
+  "🍍": "Pineapple",
+  "🍎": "Red Apple",
+  "🍏": "Green Apple",
+  "🍐": "Pear",
+  "🍒": "Cherry",
+  "🍓": "Strawberry",
+  "🥝": "Kiwi",
+  "🥑": "Avocado"
+};
+var fruitWeKnow = Object.keys(fruitDictionary);
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  function fruitInputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = fruitDictionary[userInput];
+    if (meaning === undefined) {
+      meaning = " Sorry! we don't have this in our daabase";
+    }
+    setMeaning(meaning);
+  }
+  function fruitClickHandler(fruit) {
+    var meaning = fruitDictionary[fruit];
+    setMeaning(meaning);
+  }
+  return (
+    <div className="App">
+      <h2>Know Fruits</h2>
+      <input onChange={fruitInputHandler} />
+      <h2> {meaning}</h2>
+      <h3>fruits we know</h3>
+      {fruitWeKnow.map(function (fruit) {
+        return (
+          <span
+            onClick={() => fruitClickHandler(fruit)}
+            style={{ fontSize: "2rem", padding: "1rem", cursor: "pointer" }}
+            key={fruit}
+          >
+            {" "}
+            {fruit}{" "}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
